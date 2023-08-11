2c0a02e24c41b7b0267502f958b364b5098797e8
R3ap4r <R3ap4r@telenet.be>
2023-08-11 17:50:35 +0200

    Base class where all Views will inherit from closes #8

---
+++ SimpleMaui/ViewModels/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using SimpleMaui.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMaui.ViewModels;
+public partial class BaseViewModel:ObservableObject
+{
+	//inject navigation service
+	protected readonly INavigationService _navigationService;
+	public BaseViewModel(INavigationService navigationService)
+	{
+		_navigationService = navigationService;
+	}
+}
