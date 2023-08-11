427589e742411dff164da13fe1160fce678ecaa3
R3ap4r <R3ap4r@telenet.be>
2023-08-11 17:57:16 +0200

    Let our MainviewModel inherent from Base

--- SimpleMaui/ViewModels/MainViewModel.cs
+++ SimpleMaui/ViewModels/MainViewModel.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using SimpleMaui.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SimpleMaui.ViewModels;
-public class MainViewModel
+public partial class MainViewModel: BaseViewModel
 {
+	public MainViewModel(INavigationService navigationService) : base(navigationService)
+	{
+	}
 }
