d3cbfaf528253f26b170c8bdbba89fa9aa9371e7
R3ap4r <R3ap4r@telenet.be>
2023-08-11 17:20:50 +0200

    Class implementation for the Navigation Service #5

---
+++ SimpleMaui/Services/NavigationService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMaui.Services;
+public class NavigationService: INavigationService
+{
+	/// <summary>
+	/// Go to the page with the given route and parameters if present
+	/// </summary>
+	/// <param name="route">name of the page to navigate to</param>
+	/// <param name="parameters">name of the object to pass with it</param>
+	/// <returns>Either with or without an object specified by the user</returns>
+	public Task NavigateToAsync(string route,IDictionary<string,object> parameters = null)
+	{
+		if(parameters is null)
+		{
+			return Shell.Current.GoToAsync(route);
+		}
+		else
+		{
+			return Shell.Current.GoToAsync(route,parameters);
+		}
+	}
+}
